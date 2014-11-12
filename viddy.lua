21da1a4e9bdca5ba9e385d944ca7fd6e284b1e10
Arkiver2 <Arkiver@hotmail.com>
2014-11-12 20:11:34 +0100

    viddy.lua: fix

--- viddy.lua
+++ viddy.lua
@@ -65,8 +65,10 @@ wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_pars
       or string.match(url, "/v1/")
       or string.match(url, "/static/")
       or string.match(url, "/ajax/") then
-      return true
-      addedtolist[url] == true
+      if not string.match(url, "//////") then
+        addedtolist[url] == true
+        return true
+      end
     else
       return false
     end
